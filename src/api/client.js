a5ea39aff8e76607ad9a30680de21d8cd4972d32
SHAIK SURAZ <shaik.suraz50@gmail.com>
2024-12-31 14:27:16 +0530

    changed nginx

--- src/api/client.js
+++ src/api/client.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 
 const client = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_URL}/api || http://13.60.25.3:8000/api`,
+  baseURL: import.meta.env.VITE_BASE_URL
+    ? `${import.meta.env.VITE_BASE_URL}/api`
+    : "http://13.60.25.3:8000/api",
 });
-
 
 export default client;
