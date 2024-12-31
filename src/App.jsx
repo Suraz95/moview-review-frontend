559f71830f698b8f54a2e7578d42ef86987fd828
SHAIK SURAZ <shaik.suraz50@gmail.com>
2024-12-31 11:40:26 +0530

    changed docker file

--- src/App.jsx
+++ src/App.jsx
@@ -25,36 +25,41 @@ const App = () => {
    if (isAdmin) return <AdminNavigator />;
 
    return (
-      <main className="w-full h-screen">
-         <Navbar />
-         <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Auth Routes */}
-            <Route
-               path="/auth/signIn"
-               element={
-                  <ProtectedAuth>
-                     <SignIn />
-                  </ProtectedAuth>
-               }
-            />
-            <Route
-               path="/auth/signUp"
-               element={
-                  <ProtectedAuth>
-                     <SignUp />
-                  </ProtectedAuth>
-               }
-            />
-            <Route path="/auth/email-verification" element={<EmailVerification />} />
-            <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-            <Route path="/auth/reset-password" element={<ResetPassword />} />
-            <Route path="/movie/:movieId" element={<SingleMovie />} />
-            <Route path="/movie/reviews/:movieId" element={<Reviews />} />
-            <Route path="/movie/search" element={<SearchMovieResults />} />
-            <Route path="*" element={<Notfound />} />
-         </Routes>
-      </main>
+     <main className="w-full h-screen">
+     {  console.log(`${import.meta.env.VITE_BASE_URL}`)}
+      
+       <Navbar />
+       <Routes>
+         <Route path="/" element={<Home />} />
+         {/* Auth Routes */}
+         <Route
+           path="/auth/signIn"
+           element={
+             <ProtectedAuth>
+               <SignIn />
+             </ProtectedAuth>
+           }
+         />
+         <Route
+           path="/auth/signUp"
+           element={
+             <ProtectedAuth>
+               <SignUp />
+             </ProtectedAuth>
+           }
+         />
+         <Route
+           path="/auth/email-verification"
+           element={<EmailVerification />}
+         />
+         <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+         <Route path="/auth/reset-password" element={<ResetPassword />} />
+         <Route path="/movie/:movieId" element={<SingleMovie />} />
+         <Route path="/movie/reviews/:movieId" element={<Reviews />} />
+         <Route path="/movie/search" element={<SearchMovieResults />} />
+         <Route path="*" element={<Notfound />} />
+       </Routes>
+     </main>
    );
 };
 
