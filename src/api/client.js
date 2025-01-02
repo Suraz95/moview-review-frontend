c7dbc83c37ea525334cab757f35b93d9a0c5ddad
SHAIK SURAZ <shaik.suraz50@gmail.com>
2025-01-02 10:29:43 +0530

    changed config file of nginx

--- src/api/client.js
+++ src/api/client.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL
-    ? `${import.meta.env.VITE_BASE_URL}/api`
+  baseURL: window.__RUNTIME_CONFIG__?.VITE_BASE_URL
+    ? `${window.__RUNTIME_CONFIG__.VITE_BASE_URL}/api`
     : "http://13.60.25.3:8000/api",
 });
+
 
 export default client;
