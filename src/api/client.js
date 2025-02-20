8a5707c2b6bd8ea86dddb1fbea76028c8ad1decd
SHAIK SURAZ <95890811+Suraz95@users.noreply.github.com>
2025-02-20 21:24:27 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const client = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL
     ? `${import.meta.env.VITE_BASE_URL}/api`
-    : "http://localhost:3000/api",
+    : "http://51.20.71.77:8000/api",
 });
 
 export default client;
